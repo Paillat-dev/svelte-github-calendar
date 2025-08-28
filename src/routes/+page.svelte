ca88cbbca04cdeddbf2eff1bacfd1f412280da54
Paillat <paillat@pycord.dev>
2025-08-28 19:09:27 +0200

    :art: Format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { GitHubCalendar } from '../lib/index.js';
 </script>
+
 <main>
-<GitHubCalendar username="Paillat-dev" class="w-full h-auto" />
+	<GitHubCalendar username="Paillat-dev" class="w-full h-auto" />
 </main>
